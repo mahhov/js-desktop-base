7f0e4ff37fa1b21742cfdb834826872105bf2bb8
manuk <mahhov1@gmail.com>
2019-03-27 16:08:31 -0700

    Split repo into js-desktop-base and clipboard manaager - rename clipboard.js to clipboardListener.js to match class name - remove clipboard manager and poePricer code (this is a repo split) - create index.js to export all helpers - refresh package.json

---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	keySender: require('./base/keySender/keySender'),
+	ClipboardListener: require('./base/ClipboardListener'),
+	httpRequest: require('./base/httpRequest'),
+	ScreenMouse: require('./base/ScreenMouse'),
+	ShortcutListener: require('./base/ShortcutListener'),
+	TrayHelper: require('./base/TrayHelper'),
+	ViewHandle: require('./base/ViewHandle'),
+	XPromise: require('./base/XPromise'),
+};
