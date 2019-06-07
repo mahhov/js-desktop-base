85d113f3bc4a92525949160679aee3fb5ff66510
manuk <mahhov1@gmail.com>
2019-06-07 12:14:20 -0700

    remove window.minimize() workaround for electron bug regarding foregrounding window. unsure why it is not the source of the background error whereas before it was the fix for it.

--- src/base/ViewHandle.js
+++ src/base/ViewHandle.js
@@ -47,7 +47,6 @@ class ViewHandle {
 	// if duration is falsy, will not auto-hide
 	async show(duration) {
 		this.send({name: 'open'});
-		(await this.window).minimize();
 		(await this.window).show();
 		(await this.window).restore();
 		clearInterval(this.timedHide);
