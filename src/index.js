edce5a765d973e91b732aa5a1404432472382d86
manuk <mahhov1@gmail.com>
2019-07-07 12:40:08 -0700

    make index exports lazy singletons. Were non-lazy singletons prior. - add frontWindowTitle to index.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-module.exports = {
-	appReadyPromise: require('./base/appReadyPromise'),
-	ClipboardListener: require('./base/ClipboardListener'),
-	httpRequest: require('./base/httpRequest'),
-	keyHook: require('./base/osScripts/keyHook/keyHook'),
-	keySender: require('./base/osScripts/keySender/keySender'),
-	ScreenMouse: require('./base/ScreenMouse'),
-	ShortcutListener: require('./base/ShortcutListener'),
-	TrayHelper: require('./base/TrayHelper'),
-	ViewHandle: require('./base/ViewHandle'),
-	XPromise: require('./base/XPromise'),
+let requires = {
+	appReadyPromise: './base/appReadyPromise',
+	ClipboardListener: './base/ClipboardListener',
+	frontWindowTitle: './base/osScripts/frontWindowTitle/frontWindowTitle',
+	httpRequest: './base/httpRequest',
+	keyHook: './base/osScripts/keyHook/keyHook',
+	keySender: './base/osScripts/keySender/keySender',
+	ScreenMouse: './base/ScreenMouse',
+	ShortcutListener: './base/ShortcutListener',
+	TrayHelper: './base/TrayHelper',
+	ViewHandle: './base/ViewHandle',
+	XPromise: './base/XPromise',
 };
+
+let singletonRequires = Object.entries(requires).reduce((acc, [name, path]) => Object.defineProperty(acc, name, {
+	get: function () {
+		let key = name + '_';
+		return this[key] = this[key] || require(path);
+	}
+}), {});
+
+module.exports = singletonRequires;
