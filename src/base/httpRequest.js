b8395140e9c8a74186b67415a851e98394a537f6
manuk <mahhov1@gmail.com>
2020-03-31 09:50:35 -0700

    [httpRequest], read errors from response and extract duplicate get and post response handling

--- src/base/httpRequest.js
+++ src/base/httpRequest.js
@@ -1,45 +1,48 @@
 const https = require('https');
 const querystring = require('querystring');
 
+let handleResponse = response =>
+	new Promise((resolve, reject) => {
+		let body = [];
+		response.on('data', chunk => body.push(chunk));
+		response.on('end', () => {
+			let parsed;
+			try {
+				parsed = Buffer.concat(body).toString();
+			} catch (e) {
+				reject('failed to parse', body, e);
+			}
+			if (response.statusCode < 200 || response.statusCode >= 300)
+				reject(parsed);
+			else
+				resolve(parsed);
+		});
+	});
+
 let get = (endpoint, queryParams = {}) =>
 	new Promise((resolve, reject) => {
 		let queryParamsString = querystring.stringify(queryParams);
-		https.get(queryParamsString ? `${endpoint}?${queryParamsString}` : endpoint, res => {
-			if (res.statusCode < 200 || res.statusCode >= 300)
-				reject(res);
-			let body = [];
-			res.on('data', chunk => body.push(chunk));
-			res.on('end', () => {
-				try {
-					resolve(Buffer.concat(body).toString());
-				} catch (e) {
-					reject(e);
-				}
-			});
-		}).on('error', reject);
+		let endpointWithParams = queryParamsString ? `${endpoint}?${queryParamsString}` : endpoint;
+		https.get(endpointWithParams,
+			response => handleResponse(response)
+				.then(resolve)
+				.catch(reject))
+			.on('error', reject);
 	});
 
 let post = (endpoint, data) =>
 	new Promise((resolve, reject) => {
-		let req = https.request(endpoint, {
+		let reqOptions = {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 				'Content-Length': JSON.stringify(data).length,
 			},
-		}, res => {
-			if (res.statusCode < 200 || res.statusCode >= 300)
-				reject(res.statusCode);
-			let body = [];
-			res.on('data', chunk => body.push(chunk));
-			res.on('end', () => {
-				try {
-					resolve(Buffer.concat(body).toString());
-				} catch (e) {
-					reject('failed to parse', e);
-				}
-			});
-		});
+		};
+		let req = https.request(endpoint, reqOptions,
+			response => handleResponse(response)
+				.then(resolve)
+				.catch(reject));
 		req.on('error', reject);
 		req.write(JSON.stringify(data));
 		req.end();
